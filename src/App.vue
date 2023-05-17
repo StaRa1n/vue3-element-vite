6317974b8016a95894b3fd5775b74fdd428292ee
StaRa1n <1945159551@qq.com>
2023-05-17 22:35:30 +0800

    feat: 代码提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// element默认中文
+import locale from 'element-plus/lib/locale/lang/zh-cn.js'
+</script>
+
+<template>
+  <div>
+    <el-config-provider :locale="locale">
+      <h1>这里是App</h1>
+    </el-config-provider>
+  </div>
+</template>
+
+<style scoped></style>
