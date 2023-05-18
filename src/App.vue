6447d22fe741618945bd3a1412174debd265a1b1
StaRa1n <1945159551@qq.com>
2023-05-18 22:46:54 +0800

    test: 测试配置

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
 // element默认中文
 import locale from 'element-plus/lib/locale/lang/zh-cn.js'
+
+import { onMounted } from 'vue'
+import { reqLogin } from './api/user'
+// 测试请求
+onMounted(() => {
+  reqLogin({ username: 'admin', password: '111111' })
+})
 </script>
 
 <template>
   <div>
     <el-config-provider :locale="locale">
       <h1>这里是App</h1>
+      <SvgIcon></SvgIcon>
     </el-config-provider>
+    <router-view></router-view>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+div {
+  h1 {
+    font-weight: normal;
+  }
+}
+h1 {
+  color: $color;
+}
+</style>
