87e730b1e7584054afbe3fea47acf96d49d9fb67
StaRa1n <1945159551@qq.com>
2023-05-19 23:09:18 +0800

    feat: 本地存储读写工具

---
+++ src/utils/token.ts
@@ -0,0 +1,10 @@
+// 封装本地存储数据与读取数据方法
+//存储数据
+export const SET_TOKEN = (token: string) => {
+  localStorage.setItem('TOKEN', token)
+}
+
+//获取本地存储数据
+export const GET_TOKEN = () => {
+  return localStorage.getItem('TOKEN')
+}
