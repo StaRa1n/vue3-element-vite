245e220cde5244a1dddcb343341202a0b526438d
StaRa1n <1945159551@qq.com>
2023-05-22 07:41:39 +0800

    test: 测试路由菜单

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,22 +5,63 @@ export const constantRoute = [
     path: '/login',
     component: () => import('@/views/login/index.vue'),
     name: 'login', // 命名路由
+    meta: {
+      title: '登录',
+    },
+    children: [
+      {
+        path: '/ceshi1',
+        component: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '测试1',
+        },
+      },
+    ],
   },
   {
     // 登陆成功后展示数据的路由
     path: '/',
-    component: () => import('@/views/home/index.vue'),
+    component: () => import('@/layout/index.vue'),
     name: 'layout',
+    meta: {
+      title: '主页',
+    },
+    children: [
+      {
+        path: '/ceshi1',
+        component: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '测试1',
+        },
+      },
+      {
+        path: '/ceshi2',
+        component: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '测试2',
+        },
+      },
+    ],
   },
   {
     path: '/404',
     component: () => import('@/views/404/index.vue'),
     name: '404',
+    meta: {
+      title: '404',
+    },
   },
   {
     // 任意路由
     path: '/:pathMatch(.*)*',
     redirect: '/404',
     name: 'Any',
+    meta: {
+      title: '任意路由',
+    },
   },
+  // {
+  //   path: '*',
+  //   redirect: '/login',
+  // },
 ]
