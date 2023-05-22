c356124f244b122ad1651ce74150b016c8a484ab
StaRain <1945159551@qq.com>
2023-05-22 09:21:45 +0800

    feat: 路由配置

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,39 +7,61 @@ export const constantRoute = [
     name: 'login', // 命名路由
     meta: {
       title: '登录',
+      hiddle: true,
     },
-    children: [
-      {
-        path: '/ceshi1',
-        component: () => import('@/views/home/index.vue'),
-        meta: {
-          title: '测试1',
-        },
-      },
-    ],
   },
   {
     // 登陆成功后展示数据的路由
     path: '/',
     component: () => import('@/layout/index.vue'),
     name: 'layout',
+    redirect: '/home',
     meta: {
       title: '主页',
+      hiddle: true,
     },
     children: [
       {
-        path: '/ceshi1',
+        path: '/home',
         component: () => import('@/views/home/index.vue'),
+        name: 'home',
         meta: {
-          title: '测试1',
+          title: '首页',
         },
       },
       {
-        path: '/ceshi2',
+        path: '/OfficeReq',
         component: () => import('@/views/home/index.vue'),
+        name: 'OfficeReq',
         meta: {
-          title: '测试2',
+          title: '办公审批',
         },
+        children: [
+          {
+            path: '/OfficeReq/SubmitReq',
+            component: () => import('@/views/OfficeReq/SubmitReq/index.vue'),
+            name: 'SubmitReq',
+            meta: {
+              title: '发起申请',
+            },
+          },
+          {
+            path: '/OfficeReq/SubmitReq',
+            component: () => import('@/views/OfficeReq/MyReq/index.vue'),
+            name: 'SubmitReq',
+            meta: {
+              title: '我的申请',
+            },
+          },
+          {
+            path: '/OfficeReq/SubmitReq',
+            component: () => import('@/views/OfficeReq/ManageReq/index.vue'),
+            name: 'SubmitReq',
+            meta: {
+              title: '管理申请',
+            },
+          },
+        ],
       },
     ],
   },
@@ -49,12 +71,13 @@ export const constantRoute = [
     name: '404',
     meta: {
       title: '404',
+      hiddle: true,
     },
   },
   {
     // 任意路由
     path: '/:pathMatch(.*)*',
-    redirect: '/404',
+    // redirect: '/404',
     name: 'Any',
     meta: {
       title: '任意路由',
