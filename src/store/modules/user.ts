0ca5c9ec0f150924588737995d94e709fd6b9bce
StaRain <1945159551@qq.com>
2023-05-25 17:23:09 +0800

    feat: 路由鉴权与进度条

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -47,7 +47,9 @@ let useUserStore = defineStore('User', {
       if (result.code === 200) {
         this.username = result.data.checkUser.username
         this.avatar = result.data.checkUser.avatar
+        return 'OK'
       } else {
+        return Promise.reject('获取用户信息失败')        
       }
     },
     // 退出登录
