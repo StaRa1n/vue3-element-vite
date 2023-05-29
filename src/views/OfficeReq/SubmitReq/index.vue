d3f5820951fe23b62efe4f8843a4bb2cf06f1a33
StaRa1n <1945159551@qq.com>
2023-05-29 22:58:11 +0800

    feat: 静态页面

--- src/views/OfficeReq/SubmitReq/index.vue
+++ src/views/OfficeReq/SubmitReq/index.vue
@@ -1,7 +1,51 @@
 <template>
-  <div>发起申请页面</div>
+  <el-card>
+    <el-row :gutter="0">假勤</el-row>
+    <el-row :gutter="0">
+      <el-col :span="4">请假</el-col>
+      <el-col :span="4">出差</el-col>
+      <el-col :span="4">外出</el-col>
+      <el-col :span="4">加班</el-col>
+    </el-row>
+    <el-row :gutter="0">财务</el-row>
+    <el-row :gutter="0">
+      <el-col :span="4">报销</el-col>
+      <el-col :span="4">借款</el-col>
+      <el-col :span="4">奖励</el-col>
+      <el-col :span="4">采购</el-col>
+      <el-col :span="4">活动经费</el-col>
+    </el-row>
+    <el-row :gutter="0">行政</el-row>
+    <el-row :gutter="0">
+      <el-col :span="4">会议室预定</el-col>
+      <el-col :span="4">公文流转</el-col>
+      <el-col :span="4">物品维修</el-col>
+      <el-col :span="4">用车</el-col>
+      <el-col :span="4">用章</el-col>
+      <el-col :span="4">归还</el-col>
+    </el-row>
+    <el-row :gutter="0">人事</el-row>
+    <el-row :gutter="0">
+      <el-col :span="4">入职</el-col>
+      <el-col :span="4">转正</el-col>
+      <el-col :span="4">离职</el-col>
+      <el-col :span="4">转岗</el-col>
+    </el-row>
+    <el-row :gutter="0">其他</el-row>
+    <el-row :gutter="0">
+      <el-col :span="4">通用审批</el-col>
+    </el-row>
+  </el-card>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 10px;
+  .el-col {
+    border: 1px solid bisque;
+    height: 80px;
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>这里是Home: {{ userStore.username }}</div>
+  <el-row gutter="20">
+    <el-col :span="17">
+      <el-card class="hello">你好,{{ userStore.username }}</el-card>
+      <el-card class="fastReq">快速申请</el-card>
+    </el-col>
+    <el-col :span="7">
+      <el-card class="signin">考勤打卡</el-card>
+    </el-col>
+  </el-row>
+  <el-row gutter="20">
+    <el-col :span="10">
+      <el-card class="message">消息公告</el-card>
+    </el-col>
+    <el-col :span="14">
+      <el-card class="reqList">申请列表</el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
@@ -7,12 +23,22 @@ import useUserStore from '@/store/modules/user'
 let userStore = useUserStore()
 </script>
 
-<style scoped>
-div {
-  width: 100%;
-  height: 200px;
-  border: 1px solid #c8bbbb;
-  border-radius: 25px;
-  padding: 12px;
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+  .hello {
+    margin-bottom: 20px;
+    height: 120px;
+  }
+  .signin {
+    height: 260px;
+  }
+  .message,
+  .reqList {
+    height: 300px;
+  }
+  .fastReq {
+    height: 120px;
+  }
 }
 </style>
